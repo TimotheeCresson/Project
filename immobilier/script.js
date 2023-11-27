6a3ebc572bdab6e40d1043cb95bae5afb45ea850
Timothée Cresson <timotheecresson@hotmail.fr>
2023-11-27 16:30:27 +0100

    edzs

--- immobilier/script.js
+++ immobilier/script.js
@@ -1,116 +1,105 @@
-fetch('./data.json')
-  .then(response => response.json())
-  .then(data => {
+fetch("./data.json")
+  .then((response) => response.json())
+  .then((data) => {
     const tousType = document.getElementById("tousType");
     const optionBiens = tousType.querySelectorAll("option");
     const lancerRecherche = document.querySelector(".lancerRecherche");
+    const imageMaisons = document.querySelector(".imageMaisons");
+    const appart = document.getElementById("appartement");
 
-    const appart = document.getElementById("appartement")
     console.log(appart);
-    const maison = document.getElementById("maison")
-    const terrain = document.getElementById("terrain")
+    const maison = document.getElementById("maison");
+    const terrain = document.getElementById("terrain");
+    const maisonsData = data.maison;
+    const appartementData = data.appartement;
+    const terrainData = data.terrain;
 
-
-    
-    appart.addEventListener("click", ()=> {
-      const imageMaisons = document.querySelector('.imageMaisons');
-      const maisonsData = data.maison;
-      maisonsData.forEach(maison => {
-        const imgElement = document.createElement('img');
+    appart.addEventListener("click", () => {
+      imageMaisons.innerHTML = "";
+      maisonsData.forEach((maison) => {
+        const imgElement = document.createElement("img");
         imgElement.src = `./images/immobilier/${maison.photos}`;
         imgElement.alt = maison.titre;
         imgElement.classList.add("image-maison"); // on rajoute dans la class la valeur de l'option
         imageMaisons.append(imgElement);
       });
-    })
-    
-    maison.addEventListener("click", ()=> {
-      const imageMaisons = document.querySelector('.imageMaisons');
-      const appartementData = data.appartement;
-      appartementData.forEach(appartement => {
-        const imgElement = document.createElement('img');
+    });
+
+    maison.addEventListener("click", () => {
+      imageMaisons.innerHTML = "";
+      appartementData.forEach((appartement) => {
+        const imgElement = document.createElement("img");
         imgElement.src = `./images/immobilier/${appartement.photos}`;
         imgElement.alt = appartement.titre;
-        imgElement.classList.add("image-maison"); 
+        imgElement.classList.add("image-maison");
         imageMaisons.append(imgElement);
       });
-    })
+    });
 
-    terrain.addEventListener("click", ()=> {
-      const imageMaisons = document.querySelector('.imageMaisons');
-      const terrainData = data.terrain;
-      terrainData.forEach(terrain => {
-        const imgElement = document.createElement('img');
+    terrain.addEventListener("click", () => {
+      imageMaisons.innerHTML = "";
+      terrainData.forEach((terrain) => {
+        const imgElement = document.createElement("img");
         imgElement.src = `./images/immobilier/${terrain.photos}`;
         imgElement.alt = terrain.titre;
         imgElement.classList.add("image-maison");
         imageMaisons.append(imgElement);
       });
-    })
+    });
 
-
-
-
-    lancerRecherche.addEventListener('click', () => {
-      const imageMaisons = document.querySelector('.imageMaisons');
+    lancerRecherche.addEventListener("click", () => {
+      const imageMaisons = document.querySelector(".imageMaisons");
       // enlever les images avant d'en rajouter
-      imageMaisons.innerHTML = '';
+      imageMaisons.innerHTML = "";
 
-      let selectedOption; 
+      let selectedOption;
 
-      optionBiens.forEach(option => {
-        const maisonsData = data.maison;
-        const appartementData = data.appartement;
-        const terrainData = data.terrain;
-
+      optionBiens.forEach((option) => {
         if (option.selected) {
-          selectedOption = option.value; 
+          selectedOption = option.value;
         }
 
-        if(option.value === "tousType") {
-          maisonsData.forEach(maison => {
-            const imgElement = document.createElement('img');
+        if (option.value === "tousType") {
+          maisonsData.forEach((maison) => {
+            const imgElement = document.createElement("img");
             imgElement.src = `./images/immobilier/${maison.photos}`;
             imgElement.alt = maison.titre;
             imgElement.classList.add("image-maison", option.value); // on rajoute dans la class la valeur de l'option
             imageMaisons.append(imgElement);
           });
-          appartementData.forEach(appartement => {
-            const imgElement = document.createElement('img');
+          appartementData.forEach((appartement) => {
+            const imgElement = document.createElement("img");
             imgElement.src = `./images/immobilier/${appartement.photos}`;
             imgElement.alt = appartement.titre;
-            imgElement.classList.add("image-maison", option.value); 
+            imgElement.classList.add("image-maison", option.value);
             imageMaisons.append(imgElement);
           });
-          terrainData.forEach(terrain => {
-            const imgElement = document.createElement('img');
+          terrainData.forEach((terrain) => {
+            const imgElement = document.createElement("img");
             imgElement.src = `./images/immobilier/${terrain.photos}`;
             imgElement.alt = terrain.titre;
             imgElement.classList.add("image-maison", option.value);
             imageMaisons.append(imgElement);
           });
-        }
-
-
-        else if (option.value === "maison") {
-          maisonsData.forEach(maison => {
-            const imgElement = document.createElement('img');
+        } else if (option.value === "maison") {
+          maisonsData.forEach((maison) => {
+            const imgElement = document.createElement("img");
             imgElement.src = `./images/immobilier/${maison.photos}`;
             imgElement.alt = maison.titre;
             imgElement.classList.add("image-maison", option.value); // on rajoute dans la class la valeur de l'option
             imageMaisons.append(imgElement);
           });
         } else if (option.value === "appartement") {
-          appartementData.forEach(appartement => {
-            const imgElement = document.createElement('img');
+          appartementData.forEach((appartement) => {
+            const imgElement = document.createElement("img");
             imgElement.src = `./images/immobilier/${appartement.photos}`;
             imgElement.alt = appartement.titre;
-            imgElement.classList.add("image-maison", option.value); 
+            imgElement.classList.add("image-maison", option.value);
             imageMaisons.append(imgElement);
           });
         } else if (option.value === "terrain") {
-          terrainData.forEach(terrain => {
-            const imgElement = document.createElement('img');
+          terrainData.forEach((terrain) => {
+            const imgElement = document.createElement("img");
             imgElement.src = `./images/immobilier/${terrain.photos}`;
             imgElement.alt = terrain.titre;
             imgElement.classList.add("image-maison", option.value);
@@ -120,29 +109,35 @@ fetch('./data.json')
       });
 
       // cacher toutes les images au départ
-      document.querySelectorAll('.image-maison').forEach(img => {
-        img.style.display = 'none';
+      document.querySelectorAll(".image-maison").forEach((img) => {
+        img.style.display = "none";
       });
 
       // montrer seulement les images assiociés à chaque option
-      document.querySelectorAll(`.image-maison.${selectedOption}`).forEach(img => {
-        img.style.display = 'block';
-      });
-
+      document
+        .querySelectorAll(`.image-maison.${selectedOption}`)
+        .forEach((img) => {
+          img.style.display = "block";
+        });
     });
 
-    const mediaQuery = window.matchMedia('(max-width: 619px)');
+    // imageMaisons.addEventListener("mouseover", () => {
+    //   imageMaisons.style.background = `url("./images/header/headerbg.jpg")`;
+    //   console.log(imageMaisons);
+    // });
+
+    const mediaQuery = window.matchMedia("(max-width: 619px)");
 
     function handleMediaQueryChange(mediaQuery) {
       const maisonsImages = document.querySelectorAll(".image-maison");
 
       if (mediaQuery.matches) {
-        maisonsImages.forEach(image => {
+        maisonsImages.forEach((image) => {
           image.style.display = "none";
         });
       } else {
-        maisonsImages.forEach(image => {
-          image.style.display = 'block';
+        maisonsImages.forEach((image) => {
+          image.style.display = "block";
         });
       }
     }
@@ -151,4 +146,4 @@ fetch('./data.json')
 
     handleMediaQueryChange(mediaQuery);
   })
-  .catch(error => console.error('Error fetching JSON:', error));
+  .catch((error) => console.error("Error fetching JSON:", error));
--- jeu obstacle/script.js
+++ jeu obstacle/script.js
@@ -8,34 +8,33 @@ const startGame = document.querySelector(".buttonStart");
 let jumping = false;
 let resizing = false;
 
-
 // Fonction pour démarrer le jeu
 function startGameFunction() {
-    console.log("Jeu démarre");
+  console.log("Jeu démarre");
 
-    // Démarrer l'animation des obstacles avec des délais aléatoires
-    animateObstacle(obstacle1, "animationObstacle1");
-    animateObstacle(obstacle2, "animationObstacle2");
+  // Démarrer l'animation des obstacles avec des délais aléatoires
+  animateObstacle(obstacle1, "animationObstacle1");
+  animateObstacle(obstacle2, "animationObstacle2");
 }
 
 // Fonction pour démarrer l'animation d'un obstacle avec un délai aléatoire
 function animateObstacle(obstacle, animationClass) {
-    const delay = getRandomDelay();
+  const delay = getRandomDelay();
+
+  setTimeout(() => {
+    obstacle.classList.add(animationClass);
 
     setTimeout(() => {
-        obstacle.classList.add(animationClass);
-
-        setTimeout(() => {
-            obstacle.classList.remove(animationClass);
-            // Répéter l'animation avec un nouveau délai aléatoire
-            animateObstacle(obstacle, animationClass);
-        }, 4000); // délai pour enlever ma classe
-    }, delay); // délai pour ajouter de nouveau ma classe
+      obstacle.classList.remove(animationClass);
+      // Répéter l'animation avec un nouveau délai aléatoire
+      animateObstacle(obstacle, animationClass);
+    }, 4000); // délai pour enlever ma classe
+  }, delay); // délai pour ajouter de nouveau ma classe
 }
 
 // Générer un délai aléatoire entre 2 et 5 secondes
 function getRandomDelay() {
-    return Math.floor(Math.random() * (8000 - 2000 + 1)) + 1000;
+  return Math.floor(Math.random() * (8000 - 2000 + 1)) + 1000;
 }
 
 // Événement pour commencer le jeu
@@ -43,64 +42,80 @@ startGame.addEventListener("click", startGameFunction);
 
 // Événements sur les touches de direction
 document.addEventListener("keydown", (e) => {
-    if (e.code === "ArrowUp") {
-        personnage.style.visibility = "visible";
-        personnage2.style.visibility = "hidden";
-        jump();
-    }
+  if (e.code === "ArrowUp") {
+    personnage.style.visibility = "visible";
+    personnage2.style.visibility = "hidden";
+    jump();
+  }
 });
 
 document.addEventListener("keydown", (e) => {
-    if (e.code === "ArrowDown") {
-        resize();
-        personnage2.style.visibility = "visible";
-        personnage.style.visibility = "hidden";
-    }
+  if (e.code === "ArrowDown") {
+    resize();
+    personnage2.style.visibility = "visible";
+    personnage.style.visibility = "hidden";
+  }
 });
 
 // Fonction pour le saut
 function jump() {
-    if (!jumping) {
-        jumping = true;
-        personnage.classList.add("animationPerso");
+  if (!jumping) {
+    jumping = true;
+    personnage.classList.add("animationPerso");
 
-        setTimeout(() => {
-            personnage.classList.remove("animationPerso");
-            jumping = false;
-        }, 1000);
-    }
+    setTimeout(() => {
+      personnage.classList.remove("animationPerso");
+      jumping = false;
+    }, 1000);
+  }
 }
 
 // Fonction pour redimensionner (ajuster selon vos besoins)
 function resize() {
-    if (!resizing) {
-        resizing = true;
-    }
+  if (!resizing) {
+    resizing = true;
+  }
 }
 
 // vérification obstacle touche personnage
 
 var verifObstacle1 = setInterval(function () {
-    var personnageTop = parseInt(window.getComputedStyle(personnage).getPropertyValue("top"));
-    var obstacleleft = parseInt(window.getComputedStyle(obstacle1).getPropertyValue("left"))
+  var personnageTop = parseInt(
+    window.getComputedStyle(personnage).getPropertyValue("top")
+  );
+  var obstacleleft = parseInt(
+    window.getComputedStyle(obstacle1).getPropertyValue("left")
+  );
 
-if (obstacleleft<80 && obstacleleft > 60 && personnageTop >=390) {
-    obstacle1.style.animation = "none"
-    alert("perdu")
-}
-})
+  if (obstacleleft < 80 && obstacleleft > 60 && personnageTop >= 390) {
+    obstacle1.style.animation = "none";
+    alert("perdu");
+  }
+});
 
 var verifObstacle2 = setInterval(function () {
-    var personnageTop = parseInt(window.getComputedStyle(personnage).getPropertyValue("top"));
-    var personnage2Top = parseInt(window.getComputedStyle(personnage2).getPropertyValue("top"));
-    var obstacle2left = parseInt(window.getComputedStyle(obstacle2).getPropertyValue("left"));
-    
-    // Conditions de collision pour les deux personnages
-    var collisionPersonnage1 = obstacle2left < 80 && obstacle2left > 0 && personnageTop >= 200;
-    var collisionPersonnage2 = obstacle2left < 80 && obstacle2left > 0 && personnage2Top >= 900;
+  var personnageTop = parseInt(
+    window.getComputedStyle(personnage).getPropertyValue("top")
+  );
+  var personnage2Top = parseInt(
+    window.getComputedStyle(personnage2).getPropertyValue("top")
+  );
+  var obstacle2left = parseInt(
+    window.getComputedStyle(obstacle2).getPropertyValue("left")
+  );
 
-    if (collisionPersonnage1 || collisionPersonnage2) {
-        obstacle2.style.animation = "none";
-        alert("perdu");
-    }
+  // Conditions de collision pour les deux personnages
+  var collisionPersonnage1 =
+    obstacle2left < 80 && obstacle2left > 0 && personnageTop >= 200;
+  var collisionPersonnage2 =
+    obstacle2left < 80 && obstacle2left > 0 && personnage2Top >= 900;
+
+  if (collisionPersonnage2) {
+    obstacle2.style.animation = "none";
+    alert("perdu");
+  }
+  if (collisionPersonnage1) {
+    obstacle2.style.animation = "none";
+    alert("perdu");
+  }
 });
